1323b25a6a41bb908c2d5fd2264796604ace891b
DemaPy <benderdema@gmail.com>
2024-04-11 22:56:32 +0200

    components created

--- src/components/FormElements.tsx
+++ src/components/FormElements.tsx
@@ -1,4 +1,14 @@
+import { CHECKBOX_FIELD_FORM_ELEMENT } from "./fields/CHECKBOX_FIELD";
+import { DATE_FIELD_FORM_ELEMENT } from "./fields/DATE_FIELD";
+import { NUMBER_FIELD_FORM_ELEMENT } from "./fields/NUMBER_FIELD";
+import { PARAGRAPH_FIELD_FORM_ELEMENT } from "./fields/PARAGRAPH_FIELD";
+import { SELECT_FIELD_FORM_ELEMENT } from "./fields/SELECT_FIELD";
+import { SEPARATOR_FIELD_FORM_ELEMENT } from "./fields/SEPARATOR_FIELD";
+import { SPACER_FIELD_FORM_ELEMENT } from "./fields/SPACER_FIELD";
+import { SUBTITLE_FIELD_FORM_ELEMENT } from "./fields/SUBTITLE_FIELD";
+import { TEXTAREA_FIELD_FORM_ELEMENT } from "./fields/TEXTAREA_FIELD";
 import TEXT_FIELD_FORM_ELEMENT from "./fields/TEXT_FIELD";
+import { TITLE_FIELD_FORM_ELEMENT } from "./fields/TITLE_FIELD";
 
 export type SubmitFunction = (key: string, value: string) => void;
 
@@ -8,7 +18,12 @@ export type ElementsType =
   | "TextAreaField"
   | "DateField"
   | "SelectField"
-  | "CheckboxField";
+  | "CheckboxField"
+  | "TitleField"
+  | "SubTitleField"
+  | "ParagraphField"
+  | "SeparatorField"
+  | "SpacerField";
 
 export type FormElement = {
   type: ElementsType;
@@ -47,9 +62,19 @@ type FormElementsType = {
 };
 export const FormElements: FormElementsType = {
   TextField: TEXT_FIELD_FORM_ELEMENT,
-  CheckboxField: TEXT_FIELD_FORM_ELEMENT,
-  DateField: TEXT_FIELD_FORM_ELEMENT,
-  NumberField: TEXT_FIELD_FORM_ELEMENT,
-  SelectField: TEXT_FIELD_FORM_ELEMENT,
-  TextAreaField: TEXT_FIELD_FORM_ELEMENT,
+  SubTitleField: SUBTITLE_FIELD_FORM_ELEMENT,
+
+  ParagraphField: PARAGRAPH_FIELD_FORM_ELEMENT,
+  SeparatorField: SEPARATOR_FIELD_FORM_ELEMENT,
+
+  SpacerField: SPACER_FIELD_FORM_ELEMENT,
+  TitleField: TITLE_FIELD_FORM_ELEMENT,
+
+  CheckboxField: CHECKBOX_FIELD_FORM_ELEMENT,
+  DateField: DATE_FIELD_FORM_ELEMENT,
+
+  NumberField: NUMBER_FIELD_FORM_ELEMENT,
+  SelectField: SELECT_FIELD_FORM_ELEMENT,
+
+  TextAreaField: TEXTAREA_FIELD_FORM_ELEMENT,
 };
